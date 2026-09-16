4e883f4e83f67339736661ccaf225ccf93c67843
Manas Kolhe <kolhemanas156@gmail.com>
2026-09-16 20:19:57 +0530

    Document descriptive statistics module

--- statio/descriptive/__init__.py
+++ statio/descriptive/__init__.py
@@ -1,4 +1,18 @@
+"""
+Descriptive statistics provided by Statio.
+
+This module exposes the core descriptive statistics functions
+available in the Statio library.
+"""
+
 from .mean import mean
 from .median import median
 from .mode import mode
 from .variance import variance
+
+__all__ = [
+    "mean",
+    "median",
+    "mode",
+    "variance",
+]
